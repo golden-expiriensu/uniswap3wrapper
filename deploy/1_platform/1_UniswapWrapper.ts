41f8fb4570c8171a03de57849df84dd31a90a7ae
golden-expiriensu <gundari_dan@bk.ru>
2022-12-18 16:41:38 +0700

    created uniswap wrapper base class

--- deploy/1_platform/1_UniswapWrapper.ts
+++ deploy/1_platform/1_UniswapWrapper.ts
@@ -1,25 +1,23 @@
 import { DeployFunction } from 'hardhat-deploy/types'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 
-import { DEPLOY_ARGS } from './config'
-import { verify } from './helpers'
+import { verify } from '../helpers'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
   const { deploy } = deployments
 
   const { deployer } = await getNamedAccounts()
-  const args = [DEPLOY_ARGS.CONTRACT.VALUE, DEPLOY_ARGS.CONTRACT.IMMUTABLE_VALUE]
+  const args = [(await hre.ethers.getContract("MockUniswapFactory")).address]
 
-  await deploy('Contract', {
+  await deploy('UniswapWrapper', {
     from: deployer,
     args,
     log: true,
   })
 
-  await verify(hre, 'Contract', args)
+  await verify(hre, 'UniswapWrapper', args)
 }
 
 export default func
-func.tags = ['mainnet', 'Contract']
-func.id = 'Contract'
+func.tags = ['mainnet', 'UniswapWrapper']
